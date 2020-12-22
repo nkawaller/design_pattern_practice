446264b7d50e09b5931bca1de0a8d6551d10e562
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-12-22 08:47:23 -0500

    added client code

--- observer_pattern/main.py
+++ observer_pattern/main.py
@@ -56,4 +56,22 @@ class ConcreteObserverB(Observer):
     def update(self, subject: Subject) -> None:
         if subject._state == 0 or subject._state >= 2:
             print("ConcreteObserverB: Reacted to the event")
+
+
+if __name__ == '__main__':
+
+    subject = ConcreteSubject()
+
+    observer_a = ConcreteObserverA()
+    subject.attach(observer_a)
+
+    observer_b = ConcreteObserverB
+    subject.attach(observer_b)
+
+    subject.some_business_logic()
+    subject.some_business_logic()
+
+    subject.detach(observer_a)
+
+    subject.some_business_logic()
     
