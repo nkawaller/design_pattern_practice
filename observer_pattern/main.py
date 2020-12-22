0927f2c89a89296682ba8de27e722aa604a483fa
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-12-22 08:40:29 -0500

    implemented the subject (observable)

---
+++ observer_pattern/main.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from random import randrange
+from typing import List
+
+
+class Subject(ABC):
+    @abstractmethod
+    def attach(self, observer: Observer) -> None:
+        pass
+    
+    @abstractmethod
+    def detach(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        pass
+
+class ConcreteSubject(Subject):
+
+    _state: int = None
+    _observers: List[Observer] =[]
+
+    def attach(self, observer: Observer) -> None:
+        print('Subject: Attached an observer.')
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        print('Subject: Notifying observers...')
+        for observer in self._observers:
+            observer.update(self)
+
+    def some_business_logic(self) -> None:
+        print("\nSubject: I'm doing something important.")
+        self._state = randrange(0,10)
+
+        print(f"Subject: My state has just changed to: {self._state}")
+        self.notify()
+    
