fb7f52b6760d74557fe3493879a8ee0007622ead
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-12-22 08:44:49 -0500

    implemented concrete observers

--- observer_pattern/main.py
+++ observer_pattern/main.py
@@ -46,4 +46,14 @@ class Observer(ABC):
     @abstractmethod
     def update(self, subject: Subject) -> None:
         pass
+
+class ConcreteObserverA(Observer):
+    def update(self, subject: Subject) -> None:
+        if subject._state < 3:
+            print("ConcreteObserverA: Reacted to the event")
+
+class ConcreteObserverB(Observer):
+    def update(self, subject: Subject) -> None:
+        if subject._state == 0 or subject._state >= 2:
+            print("ConcreteObserverB: Reacted to the event")
     
