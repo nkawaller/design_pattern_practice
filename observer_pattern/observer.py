5c664b62ceea161e129fe44fe2eb465aa7735fff
Nathan Kawaller <nathankawaller@pop-os.localdomain>
2020-12-26 10:10:46 -0500

    abstract Subject

--- observer_pattern/observer.py
+++ observer_pattern/observer.py
@@ -2,3 +2,25 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from random import randrange
 from typing import List
+
+
+class Subject(ABC):
+    @abstractmethod
+    def attach(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def detach(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        pass
+
+
+class ConcreteSubject(Subject):
+
+    _state: int = None
+    _observers: List[Observer] = []
+
+    def attach(self, observer: Observer)
