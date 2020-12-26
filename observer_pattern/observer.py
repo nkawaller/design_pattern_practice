4a7ef7cf3a2c58fb20709bfa178343bd89c748d8
Nathan Kawaller <nathankawaller@pop-os.localdomain>
2020-12-26 10:16:13 -0500

    concrete subject

--- observer_pattern/observer.py
+++ observer_pattern/observer.py
@@ -23,4 +23,21 @@ class ConcreteSubject(Subject):
     _state: int = None
     _observers: List[Observer] = []
 
-    def attach(self, observer: Observer)
+    def attach(self, observer: Observer) -> None:
+        print('Subject: Attached an observer.')
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        print('Subject: Notifying observers...')
+        for observer in self._observers:
+            observer.update(self)
+
+    def some_business_logic(self) -> None:
+        print("\nSubject: I'm doing some business logic")
+        self._state = randrange(0, 10)
+
+        print(f"Subject: My state has just changed to: {self._state}")
+        self.notify()
