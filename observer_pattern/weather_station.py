f4f211eb89d91f3a41ba2b4f1ec6a092258b41b4
Nathan Kawaller <nathankawaller@pop-os.localdomain>
2020-12-27 16:12:03 -0500

    abstract subject

--- observer_pattern/weather_station.py
+++ observer_pattern/weather_station.py
@@ -4,3 +4,15 @@ from random import randrange
 from typing import List
 
 
+class Subject(ABC):
+    @abstractmethod
+    def attach(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def detach(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        pass
