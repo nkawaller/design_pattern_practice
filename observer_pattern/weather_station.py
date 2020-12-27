cd68e704e0c2321edd58c554b3b9aaeea4cc5d54
Nathan Kawaller <nathankawaller@pop-os.localdomain>
2020-12-27 16:25:07 -0500

    concrete subject

--- observer_pattern/weather_station.py
+++ observer_pattern/weather_station.py
@@ -16,3 +16,28 @@ class Subject(ABC):
     @abstractmethod
     def notify(self) -> None:
         pass
+
+
+class ConcreteWeatherStation(Subject):
+
+    _state: int = None
+    _observers: List[Observer] = []
+
+    def attach(self, observer: Observer) -> None:
+        print('Weather Station: Attached an observer.')
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        print('Weather Station: Notifying observers...')
+        for observer in self._observers:
+            observer.update(self)
+
+    def some_weather_calculations(self) -> None:
+        print("\nWeather Station: I'm gathering weather data.")
+        self._state = randrange(0,10)
+
+        print(f"Weather Station: My state has just changed")
+        self.notify()
