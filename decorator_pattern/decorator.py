d0ac86fe368e280347c04f9fe81584723956a838
Nathan Kawaller <kawaller@gmail.com>
2020-12-29 10:13:01 -0500

    driver code

--- decorator_pattern/decorator.py
+++ decorator_pattern/decorator.py
@@ -29,5 +29,16 @@ class ConcreteDecoratorB(Decorator):
     def operation(self) -> str:
         return f"ConcreteDecoratorB({self.component.operation()})"
 
-class client_code(component: Component) -> None:
+def client_code(component: Component) -> None:
     print(f"RESULT: {component.operation()}", end='')
+
+if __name__ == '__main__':
+    simple = ConcreteComponent()
+    print("Client: I've got a simple component:")
+    client_code(simple)
+    print("\n")
+
+    decorator1 = ConcreteDecoratorA(simple)
+    decorator2 = ConcreteDecoratorB(decorator1)
+    print("Client: Now I've got a decorated component:")
+    client_code(decorator2)
