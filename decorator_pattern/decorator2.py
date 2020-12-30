4b145636cd5f903116950c12b443a9db5f426f5e
Nathan Kawaller <kawaller@gmail.com>
2020-12-30 17:54:35 -0500

    abstract decorator

--- decorator_pattern/decorator2.py
+++ decorator_pattern/decorator2.py
@@ -5,3 +5,15 @@ class Component:
 class ConcreteComponent(Component):
     def operation(self) -> str:
         return "This is the concrete component"
+
+class Decorator(Component):
+    _component: Component = None
+
+    def __init__(self, component: Component) -> None:
+        self._component = component
+
+        @property
+        def component(self) -> str:
+            return self._component
+        def operation(self) -> str:
+            return self._component.operation()
