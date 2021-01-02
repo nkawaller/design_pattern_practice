5c7ad4e63e70608144cd8f3939e2352942e665ea
Nathan Kawaller <kawaller@gmail.com>
2021-01-02 18:30:36 -0500

    similar interface

--- decorator_pattern/decorator3.py
+++ decorator_pattern/decorator3.py
@@ -6,16 +6,28 @@ class Add:
     def process(self):
         return self.a + self.b
 
-def modify_process(decorated_object, num):
-    return decorated_object.process()*num
+# def modify_process(decorated_object, num):
+#     return decorated_object.process()*num
 
 # Object enclosing
 class EnclosedAdd:
     def __init__(self, decorated_object):
         self.decorated_object = decorated_object
 
+# enclosed it within a decorating object with similar interface
+# create a similar interface as class Add
+class Multiply:
+    def __init__(self, decorated, num):
+        self.decorated = decorated
+        self.num = num
+
+    def process(self):
+        return self.decorated.process() * self.num
+
 ObjectA = Add(2,2)
 
 # print(ObjectA.process())
 # print(modify_process(ObjectA,4))
-enclosed_object_A = EnclosedAdd(ObjectA)
+# enclosed_object_A = EnclosedAdd(ObjectA)
+multiply_add_object = Multiply(ObjectA, 4)
+print(multiply_add_object.process())
