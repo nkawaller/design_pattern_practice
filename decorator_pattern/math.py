28cf731b9436182ae8b95348156cfc08fc1460ae
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-01-03 16:25:31 -0500

    added divide class

--- decorator_pattern/math.py
+++ decorator_pattern/math.py
@@ -8,10 +8,23 @@ class Add:
 
 # Enclose it within a decorating object with a similar interface
 class EnclosedAdd:
-    def __init__(self, decorated_object):
+    def __init__(self, decorated_object, num):
         print("I'm wrapping the add object")
         self.decorated_object = decorated_object
+        self.num = num
+
+    def process(self):
+        return self.decorated_object.process() * self.num
+
+class Divide:
+    def __init__(self.decorated_object, num):
+        self.decorated_object = decorated_object
+        self.num = num
+
+    def process(self):
+        print("I'm a divide process")
+        return self.decorated_object.process() // self.num
 
 add_object = Add(5,5)
-enclosed_add_object = EnclosedAdd(add_object)
-print(enclosed_add_object)
+enclosed_add_object = EnclosedAdd(add_object, 5)
+print(enclosed_add_object.process())
