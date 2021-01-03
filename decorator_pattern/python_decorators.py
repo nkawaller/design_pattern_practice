8c4fac448694dc4b3dd4c9c2248928d06dd17f9e
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-01-03 16:39:55 -0500

    decorating add and subtract functions

--- decorator_pattern/python_decorators.py
+++ decorator_pattern/python_decorators.py
@@ -1,1 +1,18 @@
 # Python decorators are different from the decorator pattern
+
+def multiply_by_two(func):
+    def _multiply_by_two(a, b):
+        return func(a, b) * 2
+    return _multiply_by_two
+
+@multiply_by_two
+def add(a, b):
+    return a + b
+
+@multiply_by_two
+def subtract(a, b):
+    return a - b
+
+
+print(add(10,5))
+print(subtract(60,30))
