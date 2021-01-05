a6914f14b2dd7453160f0cc05ff0e83948df3a2b
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-01-05 11:46:59 -0500

    song object

--- factory_method_pattern/factory_method.py
+++ factory_method_pattern/factory_method.py
@@ -1,2 +1,13 @@
 # Studying the factory method pattern using this article:
 # https://realpython.com/factory-method-python/
+
+# How you'd handle different situations without the factory method
+
+import json
+import xml.etree.ElementTree as et
+
+class Song:
+    def __init__(self, song_id, title, artist):
+        self.song_id = song_id
+        self.title = title
+        self.artist = artist
