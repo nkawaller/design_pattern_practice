e5a3e4d000de0710d8cb16f4ed9dfef25fb804f1
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-01-06 11:56:41 -0500

    fixed error

--- factory_method_pattern/factory_method.py
+++ factory_method_pattern/factory_method.py
@@ -5,13 +5,13 @@
 
 import json
 import xml.etree.ElementTree as et
-"""
+
 class Song:
     def __init__(self, song_id, title, artist):
         self.song_id = song_id
         self.title = title
         self.artist = artist
-
+"""
 class SongSerializer:
     def serialize(self, song, format):
         if format == 'JSON':
@@ -53,11 +53,11 @@ class SongSerializer:
 
     def _serialize_to_xml(self, song):
         song_info = et.Element('song', attrib={'id': song.song_id})
-            title = et.SubElement(song_info, 'title')
-            title.text = song.title
-            artist = et.SubElement(song_info, 'artist')
-            artist.text = song.artist
-            return et.tostring(song_info, encoding='unicode')
+        title = et.SubElement(song_info, 'title')
+        title.text = song.title
+        artist = et.SubElement(song_info, 'artist')
+        artist.text = song.artist
+        return et.tostring(song_info, encoding='unicode')
 """
 import factory_method as sd
 song = sd.Song('1', 'Water of Love', 'Dire Straits')
